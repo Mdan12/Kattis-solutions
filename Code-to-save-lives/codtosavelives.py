cb833b4c75b3779834d5a26acfc015f158a3fb6d
mdan_12 <magnusde93@gmail.com>
2023-06-25 13:09:11 +0000

    Code to Save Lives, Kattis, Python

---
+++ Code-to-save-lives/codtosavelives.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    line1 = int(input().replace(" ", ""))
+    line2 = int(input().replace(" ", ""))
+    num = line1+line2
+    num = " ".join(str(num))
+    print(num)
