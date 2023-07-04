c75b7ede2dddb5dfec6ab363163ac40182d36aa9
Magnús Daníel Budai Einarsson <magnusdbe@kopavogur.is>
2023-07-04 17:48:47 +0000

    Odd Gnome, Kattis, Python

--- Odd-gnome/oddgnome.py
+++ Odd-gnome/oddgnome.py
@@ -1,6 +1,7 @@
 n = int(input())
 for _ in range(n):
-    gnomes = list(map(int, input().split()))[1::]
-    for i in range(0,len(gnomes)-1):
-        if gnomes[i+1]-gnomes[i]!=1:
+    gnomes = list(map(int, input().split()))[1:]
+    for i in range(1,len(gnomes)-1):
+        if gnomes[i]-gnomes[i-1]!=1:
             print(i+1)
+            break
