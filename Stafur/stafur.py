e01a634590d1d59b7d4c8612da785bf22bce9c25
mdan12 <magnusde93@gmail.com>
2023-11-13 20:33:39 +0000

    Stafur, Python, Easy 1.6

---
+++ Stafur/stafur.py
@@ -0,0 +1,9 @@
+vowels = "AEIOU"
+consonant = "ZXCVBNMSDFGHJKLQWRTP"
+letter = input()
+if letter in vowels:
+    print("Jebb")
+elif letter in consonant:
+    print("Neibb")
+else:
+    print("Kannski")
